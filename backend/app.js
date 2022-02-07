1a067a54f62b19aaebc71aa5a7daddcab93a19f9
Muhammad Eid <meid95874@gmail.com>
2022-02-07 23:56:09 +0200

    setting up environment

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+// routes
+const products = require('./routes/product');
+
+app.use('/api/v1', products);
+
 
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const app = require('./app');
-
+const connectDatabase = require('./config/database');
 const dotenv = require('dotenv');
 
+
 dotenv.config({path : 'backend/config/config.env'});
+
+connectDatabase();
 
 app.listen(process.env.PORT, () => {
     console.log(`Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`);
